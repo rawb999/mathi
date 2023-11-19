961b87a4229d6035af2b43f01cfce284079cd453
GuadalupeVilla <77758787+GuadalupeVilla@users.noreply.github.com>
2023-11-19 14:31:40 -0800

    updates
    working on db

--- Assets/Scenes/Firebase_Components/FirebaseController.cs
+++ Assets/Scenes/Firebase_Components/FirebaseController.cs
@@ -8,15 +8,22 @@ public class FirebaseController : MonoBehaviour
 {
 
 
-    public GameObject loginPanel, signupPanel, forgotpasswordpanel;
+    public GameObject loginPanel, signupPanel, forgotPasswordPanel, myAccount, notificationPanel;
 
     public TMP_InputField loginEmail, forgotPasswordEmail, loginPassword, signupEmail, signupPassword, signupFname, signupLname, signupUsername;
+
+    public TMP_Text notificationTitle, notificationMessage, accountUsername, accountEmail;
+
+    public Toggle rememberMe;
+
 
     // login
     public void LoginUser()
     {
         if (string.IsNullOrEmpty(loginEmail.text) && string.IsNullOrEmpty(loginPassword.text))
         {
+            showNotification("Error", "Please enter valid Information");
+
             return;
         }
         
@@ -28,10 +35,40 @@ public class FirebaseController : MonoBehaviour
 
         if (string.IsNullOrEmpty(signupEmail.text) && string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupUsername.text))
 
+
+            showNotification("Error", "Please enter valid Information");
+
             return;
         
      }
-    
-        
 
+    public void forgotpass()
+    {
+
+        if (string.IsNullOrEmpty(forgotPasswordEmail.text))
+        {
+            showNotification("Error", "Email not Entered");
+            return;
+        }
+    
+    }
+    private void showNotification(string title, string message)
+    {
+        notificationTitle.text = "" + title;
+        notificationMessage.text = "" + message;
+
+        notificationPanel.SetActive(true);
+    }
+    public void closeNotification()
+    {
+        notificationTitle.text = "";
+        notificationMessage.text = "";
+        notificationPanel.SetActive(false);
+    
+    }
+    public void logOut()
+    {
+        myAccount.SetActive(false);
+        //openStartMenu();
+    }
 }
