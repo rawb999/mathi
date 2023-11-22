7a8865534cac0730b230f6cbe10a1088b3f665c7
GuadalupeVilla <77758787+GuadalupeVilla@users.noreply.github.com>
2023-11-21 16:15:53 -0800

    DB work
    Just working on DB so far so good!

--- Assets/Scenes/DB-webrequest/web_request.cs
+++ Assets/Scenes/DB-webrequest/web_request.cs
@@ -13,13 +13,16 @@ public class web_request : MonoBehaviour
     void Start()
     {
         // A correct website page.
-        StartCoroutine(GetRequest("http://localhost/backend-time/get.php"));  //URL of file goes here 
+        StartCoroutine(GetDate("http://localhost/backend-time/getdate.php"));  //URL of file goes here 
+        StartCoroutine(Getusertest("http://localhost/MATHi/register_users.php"));
+        StartCoroutine(Login("test@gmail.com",  "Superman"));
+
 
         // A non-existing page.
         //StartCoroutine(GetRequest("https://error.html"));
     }
 
-    IEnumerator GetRequest(string uri)
+    IEnumerator GetDate(string uri)
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) 
         {
@@ -44,4 +47,53 @@ public class web_request : MonoBehaviour
             }
         }
     }
+
+    IEnumerator Getusertest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Data processing Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+
+    IEnumerator Login(string email, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", email);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/MATHi/register_users.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+
+        }
+    }
+    //end
 }
