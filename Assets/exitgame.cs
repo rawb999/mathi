b6844e0e57b186f34076e5f88819c3068e379503
GuadalupeVilla <77758787+GuadalupeVilla@users.noreply.github.com>
2023-11-28 12:50:22 -0800

    updated things
    ldsfhol

--- Assets/exitgame.cs
+++ Assets/exitgame.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class exitgame : MonoBehaviour
 {
-    void doExitGame()
+
+
+    public Button exitButton;
+
+    void QuitGame()
     {
         Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+        exitButton.onClick.AddListener(() => { QuitGame(); });
+
     }
+
+    
+        //exitButton.onClick.AddListener(() =>{QuitGame();});
+
+    
 }
